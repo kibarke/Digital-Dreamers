9f65421d28c63a59f8c35b406af504b2559ee8e1
kibarke <kibarkework@gmail.com>
2024-03-27 21:01:13 -0400

    routes in progress, barebones

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('/'));
+// app.use(require('/routes/routes.js'));
 
 // Starts the server to begin listening
 app.listen(PORT, () => {
