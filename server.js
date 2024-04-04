0b218abbfe21c7eed538c5ffeab1179bad298928
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-04-03 21:50:16 -0400

    fixed conflict

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const routes = require('./controllers');
-<<<<<<< Updated upstream
 const session = require('express-session');
 
 
-=======
->>>>>>> Stashed changes
 const hbs = exphbs.create({});
 
 // Sets up the Express App
@@ -23,16 +20,8 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
-<<<<<<< Updated upstream
-
-
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log(`Server listening on: http://localhost:${PORT}`);
-=======
 
 // Starts the server to begin listening
 app.listen(PORT, () => {
   console.log('Server listening on: http://localhost:' + PORT);
->>>>>>> Stashed changes
 });
