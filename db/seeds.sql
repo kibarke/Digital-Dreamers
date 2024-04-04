5857f14037c98f9932bef67e78dd558d5d6fc63e
kibarke <kibarkework@gmail.com>
2024-04-03 20:36:17 -0400

    added a seeds sql file where it tracks users and exercise

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+-- User data
+
+INSERT INTO User
+(first_name, last_name, age, sex, height_imperial, weight_imperial)
+VALUES
+('Alex', 'Grossmann', '23', 'male', '5,10', '200 Ibs'),
+('Luis', 'Rivera', '25', 'male', '5,9', '193 Ibs'),
+('Rebecca', 'Doe', '26', 'female', '5,7', '174 Ibs'),
+('Kirsten', 'Barke', '54', 'female', '5,4', '150 Ibs');
+
+
+-- Exercise database
+INSERT INTO exercise
+    (name)
+VALUES
+    ('Cardio'),
+    ('Strength Training'),
+    ('Spirituality'),
+    ('Cross Fit')
