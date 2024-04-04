d4a3599984638f397ba35b7dd5619acd382ebecb
kibarke <kibarkework@gmail.com>
2024-04-04 13:30:59 -0400

    saving point

--- public/js/video.js
+++ public/js/video.js
@@ -3,14 +3,18 @@ const Handlebars = require('handlebars');
 const strengthTemplate = `
     <iframe width="560" height="315" src="{{embeddedStrengthVideoLink}}" frameborder="0" allowfullscreen></iframe>
 `;
+const yogaTemplate = `<iframe width="560" height="315" src="{{embeddedStrengthVideoLink}}" frameborder="0" allowfullscreen></iframe>
+`;
 
-const context = {
+
+
+const strengthContext = {
     embeddedStrengthVideoLink: "https://youtu.be/tj0o8aH9vJw?si=OJULLA10KlLe3SPG"
 };
 
 
 
-const compiledTemplate = Handlebars.compile(strengthTemplate);
-const result = compiledTemplate(context);
+const compiledStrengthTemplate = Handlebars.compile(strengthTemplate);
+const resultStrength = compiledStrengthTemplate(strengthContext);
 
-console.log(result);
+console.log(resultStrength);
